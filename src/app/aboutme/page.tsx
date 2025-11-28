ce6c0905a97a1987920c22d33d0dc997fbedb8b3
Vandy Zhang <vandyz5180@gmail.com>
2025-11-28 14:58:32 -0800

    feat(About): ✨ Added about page zodiac signs

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,5 +1,38 @@
 import "@/app/globals.css";
+import Image from "next/image";
+import SignCard from "../components/zodiac/signCard";
+import Snake from "@/static/pictures/snake.png";
 
 export default function AboutMe() {
-  return <main>Hello World!</main>;
+  return (
+    <main>
+      <div className="flex flex-col gap-y-6 items-center">
+        <div
+          className="font-sriracha col-start-3 col-span-1 text-center text-4xl"
+          style={{ color: "var(--color-header)", opacity: 0.7 }}
+        >
+          My Zodiac Signs
+        </div>
+        <Image
+          src={Snake}
+          alt="Snake"
+          width={126}
+          height={126}
+          className="mt-4"
+        />
+        <div className="flex flex-col items-center gap-y-6">
+          <div className="flex items-center gap-x-4">
+            <SignCard type="Sun" sign="Leo"></SignCard>
+            <SignCard type="Moon" sign="Gemini"></SignCard>
+            <SignCard type="Rising" sign="Virgo"></SignCard>
+          </div>
+          <div className="flex items-center gap-x-4">
+            <SignCard type="Venus" sign="Cancer"></SignCard>
+            <SignCard type="Mercury" sign="Virgo"></SignCard>
+            <SignCard type="Mars" sign="Sagittarius"></SignCard>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/app/components/zodiac/signCard.tsx
@@ -0,0 +1,18 @@
+import "@/app/globals.css";
+import "./signCard.css";
+
+export type SignCardProps = {
+  type: string;
+  sign: string;
+};
+
+export default function SignCard(props: SignCardProps) {
+  const { type, sign } = props;
+
+  return (
+    <div className="signCard flex flex-col items-center justify-between p-2">
+      <span className="font-stylish text-center text-2xl">{type}</span>
+      <span className="font-sriracha text-center text-2xl">{sign}</span>
+    </div>
+  );
+}
